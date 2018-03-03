c0d64c6f64064004971b415dbe750d9e530d532e
raidel123 <raidel123@hotmai.com>
2018-03-02 21:58:42 -0500

    subprocess call to curl. add first 1000 issues to analyze data.

--- stackin.py
+++ stackin.py
@@ -2,29 +2,44 @@
 
 import os
 import subprocess
-import git
 import json
+#import git
 
 from subprocess import PIPE
 
 PATH = os.getcwd()
-repos = ["pdfbox"]
+MAXRESULTS = 1000
+
+
+repo = "pdfbox"
 
 def foo():
 	print 'hello world!'
 
-def aa():
-    print "\t\ta)"
-    a = subprocess.check_output('git rev-list --count HEAD', shell=True)
-    a = a.rstrip('\n')
-    print "\t\t\tTotal Number of Commits:", a
+def IssueReport(project):
 
-gitP = subprocess.Popen(('git log --pretty="%ae%n%ce"'), shell=True, stdout=PIPE)
-    sortP = subprocess.Popen(('sort'), shell=True, stdin=gitP.stdout, stdout=PIPE)
-    uniqP = subprocess.Popen(('uniq'), shell=True, stdin=sortP.stdout, stdout=PIPE)
-    c = subprocess.check_output('wc -l', shell=True, stdin=uniqP.stdout)
-    c = c.rstrip('\n')
+	a = subprocess.check_output('curl -o ' + ("pdfbox_issues/"+repo+str(0)+".json") + ' "https://issues.apache.org/jira/rest/api/2/search?jql=project="' + project + '"&startAt=0&maxResults=1000"', shell=True)
+	a = a.rstrip('\n')
+	print a
+
+	'''
+	maxreached = False
+	page = 0
+	while not maxreached:
+		a = subprocess.check_output('curl -o' + ("pdfbox_issues/"+repo+page+".json") + '"https://issues.apache.org/jira/rest/api/2/search?jql=project="' + project + '"&startAt=' + str(page*1000) + '&maxResults=1000"', shell=True)
+		a = a.rstrip('\n')
+		print a
+	'''
+
+	'''
+	gitP = subprocess.Popen(('git log --pretty="%ae%n%ce"'), shell=True, stdout=PIPE)
+	sortP = subprocess.Popen(('sort'), shell=True, stdin=gitP.stdout, stdout=PIPE)
+	uniqP = subprocess.Popen(('uniq'), shell=True, stdin=sortP.stdout, stdout=PIPE)
+	c = subprocess.check_output('wc -l', shell=True, stdin=uniqP.stdout)
+	c = c.rstrip('\n')
 	print "\t\t\tNumber of contributors:", c
+	'''
 
 if __name__ == "__main__":
-	foo()
+	print "Downloading reported issues on JIRA for pdfbox..."
+	report = IssueReport(repo)
